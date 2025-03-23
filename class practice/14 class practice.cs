c2f3206152fe30cb78eb22365429c2db581ab436
orc-yy <whleewhlee1004@naver.com>
2025-03-23 17:27:11 +0900

    updated

---
+++ class practice/14 class practice.cs
@@ -0,0 +1,72 @@
+// namespace ConsoleApp2
+// {
+//     class Program
+    {
+        //public class Player
+        //{
+        //    private int LastHP;
+        //    public int GetLastHP() { return LastHP; }
+        //    public void SetLastHP(int hp) { LastHP = hp; }
+
+
+        //}
+
+        //public class GameController
+        //{
+        //    public static void Main()
+        //    {
+        //        Player player = new Player();
+        //        player.SetLastHP(100);
+        //        Console.WriteLine(player.GetLastHP());
+        //    }
+        //}
+
+//         public class Player
+//         {
+//             public int CurrentHP { get; set; }
+//             public string ID { get; set; }
+//         }
+
+//         public class GameController
+//         {
+//             public static void Main()
+//             {
+//                 Player player = new Player();
+//                 player.CurrentHP = 1300;
+//                 player.ID = "이원호";
+//                 Console.WriteLine($"ID : {player.ID}, HP : {player.CurrentHP}");
+//             }
+//         }
+//     }
+// }
+
+// namespace Consoleapp1
+// {
+//     class Module
+//     {
+//         public class Player
+//         {
+//             private int current_HP;
+//             public int GetCurrent_HP() 
+//             { 
+//                 return current_HP; 
+//             }
+//             public void SetCurrent_HP(int hp) 
+//             { 
+//                 current_HP = hp; 
+//             }
+
+//         }
+
+//         public class Controller
+//         {
+//             private static void Main()
+//             {
+//                 Player player = new Player();
+//                 player.SetCurrent_HP(10000);
+//                 Console.WriteLine($"Player HP : {player.GetCurrent_HP()}");
+//             }
+//         }
+
+//     }
+// }
